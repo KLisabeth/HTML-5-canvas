d74143427671a14ecc72540b0bfb14f126670e77
Mykola Marenich <marenich.mykola@gmail.com>
2020-03-21 01:26:09 +0100

    hotfix

--- listener/draw.js
+++ listener/draw.js
@@ -1,4 +1,7 @@
 const canvas = document.querySelector('#draw');
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
--- listener/drawFalse.js
+++ listener/drawFalse.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector('#draw');
+let isDrawing = false;
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener("mouseup", () => isDrawing = false);
 canvas.addEventListener("mouseout", () => isDrawing = false);

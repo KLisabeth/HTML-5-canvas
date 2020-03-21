ae57412a9fcd57020bfc4aa868ec1fa99c1fed76
Mykola Marenich <marenich.mykola@gmail.com>
2020-03-21 02:17:35 +0100

    finalTouch

--- listener/draw.js
+++ listener/draw.js
@@ -1,6 +1,6 @@
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true,
-        [lastX, lastY] = [e.offsetX, e.offsetY]; //this will update our lastX and lastY
+    [lastX, lastY] = [e.offsetX, e.offsetY]; //this will update our lastX and lastY
 });
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
